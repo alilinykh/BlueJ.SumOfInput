91fd1531b988007584aad3dbc8f077ae718ac8b8
alilinykh <aililnykh@dtcc.edu>
2019-05-22 13:24:54 -0400

    Lab Sum of Output

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-
+        Scanner userInput = new Scanner(System.in);
+        int sum = 0;
+        System.out.println("Please enter a number: ");
+        int num = userInput.nextInt();
+        for (int i = 1; i <= num; i++) {
+            sum= sum + i;
+        }
+        System.out.println("Sum of numbers from 1 to "+ num +" is " + sum);
     }
 }
